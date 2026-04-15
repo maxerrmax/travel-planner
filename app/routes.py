c7a4aebd1f35474213971f42689b6124fe0d4a8c
max.villalba <maxvillalbapons@gmail.com>
2026-04-15 16:01:59 +0200

    Added results

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
     return render_template("index.html")
+
+@main.route("/plan", methods=["POST"])
+def plan():
+    destination = request.form.get("destination")
+    days = request.form.get("days")
+
+    return render_template("result.html", destination=destination, days=days)
+
